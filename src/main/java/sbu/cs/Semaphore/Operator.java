d8298b4875cdd424b0e9b13d8237e4e951e96e13
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-05-19 19:27:48 +0330

    semaphore task done

--- src/main/java/sbu/cs/Semaphore/Operator.java
+++ src/main/java/sbu/cs/Semaphore/Operator.java
@@ -1,20 +1,32 @@
 package sbu.cs.Semaphore;
 
+import java.util.concurrent.Semaphore;
+
 public class Operator extends Thread {
 
-    public Operator(String name) {
+    private Semaphore semaphore;
+    private Resource resource;
+
+    public Operator(String name, Semaphore semaphore, Resource resource) {
         super(name);
+        this.semaphore = semaphore;
+        this.resource = resource;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++)
         {
-            Resource.accessResource();         // critical section - a Maximum of 2 operators can access the resource concurrently
+//            Resource.accessResource(getName());         // critical section - a Maximum of 2 operators can access the resource concurrently
             try {
-                sleep(500);
+                semaphore.acquire();
+                System.out.println(getName() + " " + System.currentTimeMillis());
+                Resource.accessResource(getName());
+//                sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                semaphore.release();
             }
         }
     }
--- src/main/java/sbu/cs/Semaphore/Resource.java
+++ src/main/java/sbu/cs/Semaphore/Resource.java
@@ -2,8 +2,9 @@ package sbu.cs.Semaphore;
 
 public class Resource {
 
-    public static void accessResource() {
+    public static void accessResource(String threadName) {
         try {
+//            System.out.println(threadName + " " + System.currentTimeMillis());
             Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
