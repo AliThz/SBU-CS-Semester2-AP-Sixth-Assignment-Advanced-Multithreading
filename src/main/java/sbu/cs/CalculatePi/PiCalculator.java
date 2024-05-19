4e3894cb986686289f80d7725c3f5d6050966870
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-05-19 18:49:16 +0330

    pi algorithm implemented with threadpool and lamda tasks

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -6,6 +6,7 @@ import java.math.RoundingMode;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class PiCalculator {
@@ -27,21 +28,60 @@ public class PiCalculator {
     public static String calculate(int floatingPoint) {
         // TODO
 
-        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+//        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+//
+//        BigDecimal PI = BigDecimal.ZERO;
+//        MathContext mc = new MathContext(floatingPoint + 2);
+//
+//        for (int k = 0; k <= floatingPoint; k++) {
+//            BigDecimal term = BigDecimal.valueOf(1).divide(BigDecimal.valueOf(16).pow(k), mc)
+//                    .multiply(BigDecimal.valueOf(4).divide(BigDecimal.valueOf(8L * k + 1), mc)
+//                            .subtract(BigDecimal.valueOf(2).divide(BigDecimal.valueOf(8L * k + 4), mc))
+//                            .subtract(BigDecimal.ONE.divide(BigDecimal.valueOf(8L * k + 5), mc))
+//                            .subtract(BigDecimal.ONE.divide(BigDecimal.valueOf(8L * k + 6), mc)));
+//            PI = PI.add(term);
+//        }
+//
+//        return PI.setScale(floatingPoint, BigDecimal.ROUND_DOWN).toString();
 
-        BigDecimal sum = BigDecimal.ZERO;
+
+
         MathContext mc = new MathContext(floatingPoint + 2);
+        BigDecimal PI;
+        AtomicReference<BigDecimal> result = new AtomicReference<>(BigDecimal.ZERO);
 
-        for (int k = 0; k <= floatingPoint; k++) {
-            BigDecimal term = BigDecimal.valueOf(1).divide(BigDecimal.valueOf(16).pow(k), mc)
-                    .multiply(BigDecimal.valueOf(4).divide(BigDecimal.valueOf(8L * k + 1), mc)
-                            .subtract(BigDecimal.valueOf(2).divide(BigDecimal.valueOf(8L * k + 4), mc))
-                            .subtract(BigDecimal.ONE.divide(BigDecimal.valueOf(8L * k + 5), mc))
-                            .subtract(BigDecimal.ONE.divide(BigDecimal.valueOf(8L * k + 6), mc)));
-            sum = sum.add(term);
+        int THREADS_NUMBER = 5;
+        ExecutorService executor = Executors.newFixedThreadPool(THREADS_NUMBER);
+
+        try {
+            for (int i = 0; i < THREADS_NUMBER; i++) {
+                int finalI = i;
+                executor.submit(() -> {
+                    BigDecimal threadSum = BigDecimal.ZERO;
+                    for (int k = finalI; k <= floatingPoint; k += THREADS_NUMBER) {
+                        BigDecimal term = BigDecimal.valueOf(1).divide(BigDecimal.valueOf(16).pow(k), mc)
+                                .multiply(BigDecimal.valueOf(4).divide(BigDecimal.valueOf(8 * k + 1), mc)
+                                        .subtract(BigDecimal.valueOf(2).divide(BigDecimal.valueOf(8 * k + 4), mc))
+                                        .subtract(BigDecimal.ONE.divide(BigDecimal.valueOf(8 * k + 5), mc))
+                                        .subtract(BigDecimal.ONE.divide(BigDecimal.valueOf(8 * k + 6), mc)));
+                        threadSum = threadSum.add(term);
+                    }
+                    result.getAndAccumulate(threadSum, BigDecimal::add);
+                });
+            }
+        } finally {
+            executor.shutdown();
+            try {
+                executor.awaitTermination(1, TimeUnit.MINUTES);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
-        return sum.setScale(floatingPoint, BigDecimal.ROUND_DOWN).toString();
+
+        PI = result.get().setScale(floatingPoint, BigDecimal.ROUND_DOWN);
+
+        return PI.toString();
     }
 
     public static void main(String[] args) {
